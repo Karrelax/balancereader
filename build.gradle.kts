f09ccac6e813c6c8eda382c0dd9b1efd5a961a57
Karrelax <69399715+Karrelax@users.noreply.github.com>
2026-04-24 11:49:15 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,4 @@ plugins {
     id("com.android.application") version "7.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
 }
-compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-kotlinOptions {
-    jvmTarget = "17"
-}
+
